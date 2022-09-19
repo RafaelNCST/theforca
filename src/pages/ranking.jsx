947121dd7519634198aa67d45f06225e3f1b3322
rafaelnc <rafaelnccolares@gmail.com>
2022-09-19 17:54:13 -0300

    style: modify loading screen

--- src/pages/ranking.jsx
+++ src/pages/ranking.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import PencilLoading from '../components/loadingPencil';
 import { BodyScreen, LogoImage } from '../styles/indexStyle';
+import { Text } from '../styles/globalStyles';
 
 const Ranking = () => {
   return (
     <BodyScreen>
       <LogoImage src='/images/Logo.png' />
       <PencilLoading />
+      <Text fontSize={35} color='#000000'>
+        Carregando
+      </Text>
     </BodyScreen>
   );
 };
