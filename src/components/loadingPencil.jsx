cf96e723e8f1e6578f15f31f5f10f8d824195d8b
rafaelnc <rafaelnccolares@gmail.com>
2022-09-19 17:54:39 -0300

    style: modify loading pencil

--- src/components/loadingPencil.jsx
+++ src/components/loadingPencil.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import Lottie from 'react-lottie';
 import animationData from '../../public/lottie/loadingpencil.json';
 
+import styled from 'styled-components';
+
 const defaultOptions = {
   animationData: animationData,
   loop: true,
   autoPlay: true,
+  resizeMode: 'cover',
 };
+
+const ContainerAnimation = styled.div`
+  margin-bottom: -180px;
+`;
 
 const PencilLoading = () => {
   return (
-    <div>
+    <ContainerAnimation>
       <Lottie options={defaultOptions} height={300} width={300} />
-    </div>
+    </ContainerAnimation>
   );
 };
 
