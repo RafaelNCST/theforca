a7e7a8659616b6890889dbab0040d98852e0709b
rafaelnc <rafaelnccolares@gmail.com>
2022-09-20 20:59:32 -0300

    fix: fixed bug FOUC in menu

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import {
   faPencil,
   faRankingStar,
@@ -10,8 +10,6 @@ import GoogleButton from '../components/googleButton';
 import ButtonMenu from '../components/MenuComponents/ButtonsMenu';
 import ButtonModal from '../components/MenuComponents/ButtonModal/ButtonModal';
 import Modal from '../components/MenuComponents/ButtonModal/Modal';
-
-import LoadingPencil from '../components/loadingPencil';
 
 import MenuDropDown from '../components/MenuComponents/MenuDropDown';
 import {
@@ -39,90 +37,93 @@ const Login = () => {
   const [showModal, setShowModal] = useState(false);
   const [controlDropDown, setControlDropDown] = useState(null);
   const [activeDropDown, setActiveDropDown] = useState(false);
-  const [finishLoading, setFinishLoading] = useState(false);
+  const isMounted = useRef(false);
+  const [finishLoading] = useState(isMounted);
+  const [finishMounted, setFinishMounted] = useState(false);
 
   useEffect(() => {
-    setFinishLoading(true);
-  }, []);
+    if (finishLoading) {
+      setTimeout(() => setFinishMounted(true), 500);
+    }
+  }, [finishLoading]);
 
   return (
-    <BodyScreen>
-      <GoogleButton />
+    <>
+      {!!finishMounted && (
+        <BodyScreen ref={isMounted}>
+          <GoogleButton />
 
-      <LogoImage src='/images/Logo.png' alt='Logo do The Forca' />
+          <LogoImage src='/images/Logo.png' alt='Logo do The Forca' />
 
-      <Modal showModal={showModal} setShowModal={setShowModal} />
+          <Modal showModal={showModal} setShowModal={setShowModal} />
 
-      {/* eslint-disable-next-line no-extra-boolean-cast */}
-      {!!finishLoading ? (
-        <Menu>
-          <ContainerTop>
-            <Text color='#ffffff' fontSize={40}>
-              INÍCIO
-            </Text>
-          </ContainerTop>
-          <ContainerDropDown>
-            <MenuDropDown
-              placeholder={PLACEHOLDER_MODE_GAME}
-              zindex={3}
-              top={60}
-              array={MODE_GAME}
-              controlDropDown={controlDropDown}
-              setControlDropDown={setControlDropDown}
-              activeDropDown={activeDropDown}
-              setActiveDropDown={setActiveDropDown}
-              numberDropDown={0}
-            />
-            <MenuDropDown
-              placeholder={PLACEHOLDER_TEMA}
-              zindex={2}
-              top={110}
-              array={TEMA}
-              controlDropDown={controlDropDown}
-              setControlDropDown={setControlDropDown}
-              activeDropDown={activeDropDown}
-              setActiveDropDown={setActiveDropDown}
-              numberDropDown={1}
-            />
-            <MenuDropDown
-              placeholder={PLACEHOLDER_TIME_ROUND}
-              zindex={1}
-              top={160}
-              array={TIME_ROUND}
-              controlDropDown={controlDropDown}
-              setControlDropDown={setControlDropDown}
-              activeDropDown={activeDropDown}
-              setActiveDropDown={setActiveDropDown}
-              numberDropDown={2}
-            />
-          </ContainerDropDown>
-          <ContainerMid>
-            <ButtonModal
-              height={35}
-              icon={faQuestion}
-              text='COMO JOGAR'
-              setShowModal={setShowModal}
-            />
-            <ButtonMenu
-              router='/ranking'
-              height={35}
-              icon={faRankingStar}
-              text='RANKING'
-            />
-          </ContainerMid>
-          <ContainerBottom>
-            <ButtonMenu
-              router='/game'
-              height={40}
-              icon={faPencil}
-              text='JOGAR'
-            />
-          </ContainerBottom>
-        </Menu>
-      ) : (
-        <LoadingPencil />
+          <Menu>
+            <ContainerTop>
+              <Text color='#ffffff' fontSize={40}>
+                INÍCIO
+              </Text>
+            </ContainerTop>
+            <ContainerDropDown>
+              <MenuDropDown
+                placeholder={PLACEHOLDER_MODE_GAME}
+                zindex={3}
+                top={60}
+                array={MODE_GAME}
+                controlDropDown={controlDropDown}
+                setControlDropDown={setControlDropDown}
+                activeDropDown={activeDropDown}
+                setActiveDropDown={setActiveDropDown}
+                numberDropDown={0}
+              />
+              <MenuDropDown
+                placeholder={PLACEHOLDER_TEMA}
+                zindex={2}
+                top={110}
+                array={TEMA}
+                controlDropDown={controlDropDown}
+                setControlDropDown={setControlDropDown}
+                activeDropDown={activeDropDown}
+                setActiveDropDown={setActiveDropDown}
+                numberDropDown={1}
+              />
+              <MenuDropDown
+                placeholder={PLACEHOLDER_TIME_ROUND}
+                zindex={1}
+                top={160}
+                array={TIME_ROUND}
+                controlDropDown={controlDropDown}
+                setControlDropDown={setControlDropDown}
+                activeDropDown={activeDropDown}
+                setActiveDropDown={setActiveDropDown}
+                numberDropDown={2}
+              />
+            </ContainerDropDown>
+            <ContainerMid>
+              <ButtonModal
+                height={35}
+                icon={faQuestion}
+                text='COMO JOGAR'
+                setShowModal={setShowModal}
+              />
+              <ButtonMenu
+                router='/ranking'
+                height={35}
+                icon={faRankingStar}
+                text='RANKING'
+              />
+            </ContainerMid>
+            <ContainerBottom>
+              <ButtonMenu
+                router='/game'
+                height={40}
+                icon={faPencil}
+                text='JOGAR'
+              />
+            </ContainerBottom>
+          </Menu>
+        </BodyScreen>
       )}
-    </BodyScreen>
+    </>
   );
 };
 
