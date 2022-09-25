91c7bd4f0db499248984ff2c23f89dabb1d22f06
rafaelnc <rafaelnccolares@gmail.com>
2022-09-25 18:33:57 -0300

    ver1.0

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,12 +3,12 @@ import { GlobalStyle } from '../styles/globalStyles';
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <AuthProvider>
+    <>
       <link rel='preload' as='font' href='/fonts/VerySimpleChalk.ttf' />
       <link rel='preload' as='image' href='/images/Background.svg' />
       <GlobalStyle />
       <Component {...pageProps} />
-    </AuthProvider>
+    </>
   );
 };
 
